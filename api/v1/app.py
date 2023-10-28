32df9102f5610c94f55f0d1b1d456aca53a2404a
tobitprince <prince.tobit@strathmore.edu>
2023-10-28 18:59:54 +0300

    pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,12 +22,6 @@ def close_db(error):
 
 @app.errorhandler(404)
 def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:
-        description: a resource was not found
-    """
     return make_response(jsonify({'error': "Not found"}), 404)
 
 app.config['SWAGGER'] = {
@@ -39,7 +33,7 @@ Swagger(app)
 
 
 if __name__ == "__main__":
-    """ Main Function """
+    """ Main Function."""
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
