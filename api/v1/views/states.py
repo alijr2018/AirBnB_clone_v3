81fc67a11dab80def9d4109c33cf312950813c90
alijr2018 <alijr_2018@proton.me>
2023-10-30 19:54:39 +0100

    done

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -13,7 +13,7 @@ def get_states():
     list_states = []
     for state in all_states:
         list_states.append(state.to_dict())
-    return jsonify(list_states)
+    return (jsonify(list_states))
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
