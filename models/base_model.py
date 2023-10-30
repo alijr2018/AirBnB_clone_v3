98cd5be3723becdc5b5281fe7b43bd6f1e9f4a67
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-10-30 20:15:32 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contain class BaseModel.
+Contains class BaseModel
 """
 
 from datetime import datetime
@@ -48,31 +48,31 @@ class BaseModel:
             self.updated_at = self.created_at
 
     def __str__(self):
-        """String representation of the BaseModel class."""
+        """String representation of the BaseModel class"""
         return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
                                          self.__dict__)
 
     def save(self):
-        """Update the attribute 'updated_at' with the current datetime."""
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_hash):
-        """Return a dictionary containing all keys/values of the instance."""
+    def to_dict(self, save_fs=None):
+        """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
             new_dict["created_at"] = new_dict["created_at"].strftime(time)
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:    
+        if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if save_hash is None:
+        if save_fs is None:
             if "password" in new_dict:
                 del new_dict["password"]
         return new_dict
 
     def delete(self):
-        """Delete the current instance from the storage."""
+        """delete the current instance from the storage"""
         models.storage.delete(self)
