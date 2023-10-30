89fcc88828a5ff6290e13a9b42946daf3077f775
alijr2018 <alijr_2018@proton.me>
2023-10-30 15:18:42 +0100

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-""" Flask Application."""
+"""Flask Application."""
+from os import environ
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Close Storage."""
+    """Close Storage."""
     storage.close()
-
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
-
-if __name__ == "__main__":
-    """ Main Function."""
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+if __name__ == "__main":
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
