33af5ab4a017f4f93f8f6e9c89338fa7c11260d9
alijr2018 <alijr_2018@proton.me>
2023-10-30 15:18:59 +0100

    done

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,7 +20,7 @@ def close_db(error):
 def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
-if __name__ == "__main":
+if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
