26962d0c81692d27ed6101793fb98306518ab9c1
tobitprince <prince.tobit@strathmore.edu>
2023-10-30 10:05:34 +0300

    import hash lib

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the FileStorage class
+Contain the FileStorage class.
 """
 
 import json
@@ -12,6 +12,7 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from hashlib import md5
 
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
