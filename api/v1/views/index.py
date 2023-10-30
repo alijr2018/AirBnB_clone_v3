50dcc3ddb86c1f7eb61da241cda89550ce6118cd
alijr2018 <alijr_2018@proton.me>
2023-10-30 15:55:08 +0100

    done

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -17,7 +17,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def number_objects():
     """ Retrieves the number of each objects by type. """
     stats = {
