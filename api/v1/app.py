147272ba25c8eb1f077745b421d55514a17394d8
tobitprince <prince.tobit@strathmore.edu>
2023-10-30 09:10:51 +0300

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,6 @@ def not_found(error):
     return make_response(jsonify({'error': "Not found"}), 404)
 
 
-
 if __name__ == "__main__":
     """ Main Function."""
     host = environ.get('HBNB_API_HOST')
