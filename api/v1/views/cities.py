d599daaef2a1e81b2c8af5dc464cdbc21c8a5e12
alijr2018 <alijr_2018@proton.me>
2023-10-30 20:14:27 +0100

    done

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -30,7 +30,7 @@ def get_city(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    return jsonify(city.to_dict())
+    return (jsonify(city.to_dict()))
 
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
@@ -39,13 +39,12 @@ def delete_city(city_id):
     Delete a city based on id provided.
     """
     city = storage.get(City, city_id)
-
     if not city:
         abort(404)
     storage.delete(city)
     storage.save()
 
-    return make_response(jsonify({}), 200)
+    return (make_response(jsonify({}), 200))
 
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'],
@@ -66,7 +65,7 @@ def post_city(state_id):
     instance = City(**data)
     instance.state_id = state.id
     instance.save()
-    return make_response(jsonify(instance.to_dict()), 201)
+    return (make_response(jsonify(instance.to_dict()), 201))
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
@@ -82,10 +81,9 @@ def put_city(city_id):
         abort(400, description="Not a JSON")
 
     ignore = ['id', 'state_id', 'created_at', 'updated_at']
-
     data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
             setattr(city, key, value)
     storage.save()
-    return make_response(jsonify(city.to_dict()), 200)
+    return (make_response(jsonify(city.to_dict()), 200))
