2f3a609df6ae68cf8e520b935b2f250be6c7cba1
alijr2018 <alijr_2018@proton.me>
2023-10-30 19:50:10 +0100

    done

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,9 +8,7 @@ from flask import abort, jsonify, make_response, request
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """
-    Retrieve the list of all State objects.
-    """
+    """ Retrieve the list of all State objects. """
     all_states = storage.all(State).values()
     list_states = []
     for state in all_states:
@@ -31,10 +29,7 @@ def get_state(state_id):
 @app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_state(state_id):
-    """
-    Delete a State Object.
-    """
-
+    """ Delete a State Object. """
     state = storage.get(State, state_id)
 
     if not state:
@@ -48,9 +43,7 @@ def delete_state(state_id):
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
-    """
-    Create a State.
-    """
+    """ Create a State. """
     if not request.get_json():
         abort(400, description="Not a JSON")
 
@@ -65,9 +58,7 @@ def create_state():
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
-    """
-    Update a State.
-    """
+    """ Update a State. """
     state = storage.get(State, state_id)
 
     if not state:
