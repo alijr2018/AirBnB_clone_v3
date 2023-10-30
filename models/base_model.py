b8d2d0839e4b6e4c142de478db4924584e8aea43
tobitprince <prince.tobit@strathmore.edu>
2023-10-30 09:27:46 +0300

    Security improvements

--- models/base_model.py
+++ models/base_model.py
@@ -58,7 +58,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_hash):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -66,8 +66,11 @@ class BaseModel:
         if "updated_at" in new_dict:
             new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
         new_dict["__class__"] = self.__class__.__name__
-        if "_sa_instance_state" in new_dict:
+        if "_sa_instance_state" in new_dict:    
             del new_dict["_sa_instance_state"]
+        if save_hash is None:
+            if "password" in new_dict:
+                del new_dict["password"]
         return new_dict
 
     def delete(self):
